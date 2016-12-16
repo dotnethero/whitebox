95838b73f599cb2b5c72b44b534386827c7b4b8a
Andrew Gorbokon <andrew.gorbokon@gmail.com>
2016-12-16 04:47:45 +0200

    Add project files.

---
+++ fhg/BitReader.fs
@@ -0,0 +1,36 @@
+﻿module BitEx
+
+open System
+open System.IO
+
+type Channel = char
+type BufferSize = uint32
+type ChannelOutput =
+    | Output of Channel * BufferSize * string
+    | Input of Channel * BufferSize
+    | Exit
+
+type BinaryReader with
+    member this.ReadChannel(): Channel = this.ReadChar()
+    member this.ReadLength(): BufferSize =
+        let bytes = Array.init 4 (fun x -> byte 0)
+        this.Read(bytes, 0, 4) |> ignore
+        Array.Reverse(bytes)
+        BitConverter.ToUInt32(bytes, 0)
+    member this.ReadData(len) =
+        let data = this.ReadBytes(int len);
+        System.Text.Encoding.UTF8.GetString(data);
+
+    member this.ReadOutput(): ChannelOutput =
+        let channel = this.ReadChannel()
+        let len = this.ReadLength()
+        match channel with
+            | 'L' | 'I' -> 
+                Input (channel, len)
+            | 'o' | 'e' -> 
+                let data = this.ReadData(len)
+                Output (channel, len, data)
+            | _ -> Exit
+
+        
+
---
+++ fhg/Program.fs
@@ -0,0 +1,85 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+// import sys, struct, subprocess
+// 
+// # connect to the server
+// server = subprocess.Popen(['hg', '--config', 'ui.interactive=True', 'serve', '--cmdserver', 'pipe'],
+//                           stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+// 
+
+open System
+open System.IO
+open System.Diagnostics
+open BitEx
+
+let start (exe, cmdline) =
+    let psi = new ProcessStartInfo(exe,cmdline) 
+    psi.UseShellExecute <- false
+    psi.RedirectStandardOutput <- true
+    psi.RedirectStandardInput <- true
+    psi.RedirectStandardError <- true
+    psi.CreateNoWindow <- false
+    let p = Process.Start(psi) 
+    let reader = new BinaryReader(p.StandardOutput.BaseStream)
+    let writer = new BinaryWriter(p.StandardInput.BaseStream)
+    let readChunk() =
+        match reader.ReadOutput() with
+            | Output (channel, _, data) ->
+                printf "%c\n" channel
+                printf "%s\n" data
+            | Input (channel, _) ->
+                printf "Input:"
+    
+    let hello = readChunk()
+    Console.ReadKey
+
+// def readchannel(server):
+//     channel, length = struct.unpack('>cI', server.stdout.read(5))
+//     if channel in 'IL': # input
+//         return channel, length
+//     return channel, server.stdout.read(length)
+// 
+
+[<EntryPoint>]
+let main argv = 
+    start ("hg", "--config ui.interactive=True serve --cmdserver pipe") |> ignore
+    0 // return an integer exit code
+
+
+
+// def writeblock(data):
+//     server.stdin.write(struct.pack('>I', len(data)))
+//     server.stdin.write(data)
+//     server.stdin.flush()
+// 
+// # read the hello block
+// hello = readchannel(server)
+// print "hello block:", repr(hello)
+// 
+// # write the command
+// server.stdin.write('runcommand\n')
+// writeblock('\0'.join(sys.argv[1:]))
+// 
+// # receive the response
+// while True:
+//     channel, val = readchannel(server)
+//     if channel == 'o':
+//         print "output:", repr(val)
+//     elif channel == 'e':
+//         print "error:", repr(val)
+//     elif channel == 'r':
+//         print "exit code:", struct.unpack(">l", val)[0]
+//         break
+//     elif channel == 'L':
+//         print "(line read request)"
+//         writeblock(sys.stdin.readline(val))
+//     elif channel == 'I':
+//         print "(block read request)"
+//         writeblock(sys.stdin.read(val))
+//     else:
+//         print "unexpected channel:", channel, val
+//         if channel.isupper(): # required?
+//             break
+// 
+// # shut down the server
+// server.stdin.close()
